3510ae6bd8c63aaa319b96cdcd1e8e5c169bb8ba
Innocent Onyemaenu <[innocent.o@e360africa.com]>
2018-04-12 17:46:47 +0100

    added CI tools

--- ci-cd.sh
+++ ci-cd.sh
@@ -16,9 +16,9 @@ ssh root@185.130.207.215 <<-EOF
     mv ./temp ./live
     mkdir ./temp # create new temp directory for next deployment
     cp ./lara-config/.env ./live
-    #cp ./config/env.js ./live/public/js
     cd ./live
     composer install --no-dev --optimize-autoloader --no-plugins --no-scripts
+    composer update
     php artisan key:generate
     #php artisan route:cache resolve closure based routes before caching
     php artisan migrate --force
