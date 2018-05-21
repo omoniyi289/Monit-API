ccfa21a8cd450d520379c77fe184d735fcd41cde
Innocent Onyemaenu <[innocent.o@e360africa.com]>
2018-05-21 20:55:55 +0100

    .sh changed

--- deployment.sh
+++ deployment.sh
@@ -20,12 +20,11 @@ ssh -i /var/lib/jenkins/.ssh/e360_prod_kp2.pem -o StrictHostKeyChecking=no ubunt
     cd ./live
     sudo composer install --no-dev --optimize-autoloader --no-plugins --no-scripts
     sudo composer update
-    
     sudo php artisan key:generate
     #sudo php artisan route:cache #resolve closure based routes before caching
     sudo php artisan migrate --force
     sudo php artisan db:seed
     
     sudo chmod -R 755 .
-    #sudo chown -R www-data:www-data .
+    sudo chown -R www-data:www-data .
 EOF
